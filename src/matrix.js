b830892be1698da00391d9f056c464add32560d5
Joel Correa <jcorrear468@gmail.com>
2024-01-25 22:56:48 -0300

    epic fix para class matrix

--- src/matrix.js
+++ src/matrix.js
@@ -3,19 +3,19 @@ class Matrix {
   y;
   z;
   matrix;
-    /** 
-   *@param {Object} position - a object
-    @param {number} position.x - x axis
-     @param {number} position.y - y axis
-      @param {number} [position.z] - z axis
-   *
-   */
+  /** 
+ *@param {Object} position - a object
+  @param {number} position.x - x axis
+   @param {number} position.y - y axis
+    @param {number} [position.z] - z axis
+ *
+ */
   constructor(a) {
     if (a === undefined) {
-    this.matrix = {}
-    return this
+      this.matrix = {}
+      return this
     }
-    const {x,y,z} = a
+    const { x, y, z } = a
     this.x = x;
     this.y = y;
     if (z) this.z = z;
@@ -58,59 +58,61 @@ class Matrix {
     // Clonar la matriz de manera profunda
     const draft = deepClone(this.matrix.body);
     if ('z' in position) {
-    
-   // Verificar que la posición sea válida para el eje z
-        if (position.z > draft[0][0].length || position.z <= 0) {
-            throw new Error('Invalid position for z axis');
-        }
 
-        draft[position.y - 1][position.x - 1][position.z - 1] = [value];
+      // Verificar que la posición sea válida para el eje z
+      if (position.z > draft[0][0].length || position.z <= 0) {
+        throw new Error('Invalid position for z axis');
+      }
 
-        const clone = structuredClone(draft);
-        this.matrix.body = clone
+      draft[position.y - 1][position.x - 1][position.z - 1] = [value];
+
+      const clone = structuredClone(draft);
+      this.matrix.body = clone
 
       return this;
     } else {
-        draft[position.y-1][position.x-1] = [value]
-  
-      
-      this.matrix.body[position.y - 1] = draft[position.y-1]
+      draft[position.y - 1][position.x - 1] = [value]
+
+
+      this.matrix.body[position.y - 1] = draft[position.y - 1]
       return this;
     }
   }
   print() {
-    return this.matrix.body;
+    const ref = this.matrix.body;
+    const clone = JSON.parse(JSON.stringify(ref));
+    return clone
   }
   create(matrix) {
-  if (typeof matrix !== 'object') {
-    throw new Error('parameter must be a matrix');
-  }
+    if (typeof matrix !== 'object') {
+      throw new Error('parameter must be a matrix');
+    }
 
-  const first_axis = matrix.length;
-  const second_axis = matrix[0].length;
-  const third_axis = matrix[0][0].length;
-   
-       const first_validation = matrix.every((x) => x.length === second_axis )
-      const second_validation = matrix.every((x) => {
-        return x.every((z) => z.length === third_axis )
-      })
-   
+    const first_axis = matrix.length;
+    const second_axis = matrix[0].length;
+    const third_axis = matrix[0][0].length;
 
-  if (!first_validation && !second_validation) {
-    throw new Error('all elements must have same length');
-  }
-  const is3D = typeof matrix[0][0][0]?.length !== undefined;
+    const first_validation = matrix.every((x) => x.length === second_axis)
+    const second_validation = matrix.every((x) => {
+      return x.every((z) => z.length === third_axis)
+    })
 
-  const axis = {
-    y: first_axis,
-    x: second_axis,
-    ...(is3D ? { z: third_axis } : null),
-  };
+
+    if (!first_validation && !second_validation) {
+      throw new Error('all elements must have same length');
+    }
+    const is3D = typeof matrix[0][0][0]?.length !== "undefined";
+
+    const axis = {
+      y: first_axis,
+      x: second_axis,
+      ...(is3D ? { z: third_axis } : null),
+    };
     this.matrix.axis = axis;
     this.matrix.body = matrix;
-    return this
-}
-  
+    return this
+  }
+
 }
 
 export default Matrix
--- src/views/Calendary.jsx
+++ src/views/Calendary.jsx
@@ -1,53 +1,123 @@
-import React, {useCallback, useState} from 'react'
-import Matrix from "../matrix"
+import { useCallback, useState, useEffect, useMemo } from "react";
+import Matrix from "../matrix";
+
+
+
+//simula data obtenida de una api
+
+const mockData = [
+  {date: "2024-01-01",
+  color: "#09f09f",},
+  {date: "2024-01-02",
+  color: "#09f09f",},
+  {date: "2024-01-03",
+  color: "#09f09f",},
+  {date: "2024-01-04",
+  color: "#09f09f",},
+]
+
+const multiFill = (matrix, mockData) => {
+  matrix.solid()
+  mockData.map(({date, color}) => {
+    const [, x, y] = date.split('-')
+    matrix.fill({x: Number(x), y: Number(y) +1}, {color})
+    console.log(x, y)
+  })
+  return matrix.print()
+}
+
+
 
 function Calendary() {
-  
-  const initialMatrix = new Matrix({x: 12, y: 32}).solid().print()
-  
-  const [matrix, setMatrix] = useState(initialMatrix)
-  
+  const initialMatrix = useMemo(() => new Matrix({ x: 12, y: 32 }), [] ) 
+
+  const [matrix, setMatrix] = useState([]);
+
+  useEffect(() => {
+
+    const body = multiFill(initialMatrix, mockData)
+    setMatrix(body);
+  }, [initialMatrix]) 
+
   function cantDays(year, month) {
-    month--
-    const monthDay = new Date(year, month + 1, 0)
-    return monthDay.getDate()
+    month--;
+    const monthDay = new Date(year, month + 1, 0);
+    return monthDay.getDate();
   }
-  const onClick = useCallback((indexX, indexY) => {
-    const newMatrix = new Matrix().create(matrix).fill({x: indexX + 1, y: indexY + 1}, 1)
-    const body = newMatrix.print()
-    setMatrix(body)
-  }, []) 
+  const onClick = useCallback(
+    (indexX, indexY) => {
+      console.log(indexX+1, indexY + 1)
+      const newMatrix = new Matrix()
+        .create(matrix)
+        .fill({ x: indexX + 1, y: indexY + 1 }, { color: "#09f09f" });
+      const body = newMatrix.print();
+      setMatrix(body);
+      //get real date 
+      const date = new Date(2024, indexX, indexY).toISOString().slice(0, 10);
+      console.log(date);
+    },
+    [matrix]
+  );
 
-  console.log(matrix)
+  const CalendaryCell = useCallback(
+    ({ axisY, indexY }) =>
+      axisY.map(([axisX], indexX) => {
+        if (indexY === 0) {
+          return (
+            <p
+              key={`header-${indexX}`}
+              className="w-9 h-9 border capitalize text-center flex items-center justify-center"
+            >
+              {new Date(2024, indexX + 1, 0).toLocaleDateString("ES-AR", {
+                month: "short",
+              })}
+            </p>
+          );
+        } else {
+          return cantDays(2024, indexX + 1) >= indexY ? (
+            <p
+              key={`cell-${indexX}-${indexY}`}
+              onClick={() => onClick(indexX, indexY)}
+              className="w-9 h-9 border"
+              style={{ backgroundColor: axisX?.color }}
+            >
+              
+            </p>
+          ) : (
+            <p
+              key={`empty-${indexX}-${indexY}`}
+              className="w-9 h-9 border bg-slate-300"
+            ></p>
+          );
+        }
+      }),
+    [onClick]
+  );
 
-  const CalendaryCell = useCallback(({axisY, indexY}) => 
-  axisY.map((axisX, indexX) => {
-    if (indexY === 0) {
-      return (<p key={`header-${indexX}`} className='w-9 h-9 border capitalize text-center flex items-center justify-center'>{new Date(2024, indexX + 1, 0).toLocaleDateString('ES-AR', { month: 'short' })}</p>)
-    } else {
-      return cantDays(2024, indexX + 1) >= indexY ? <p key={`cell-${indexX}-${indexY}`} onClick={() => onClick(indexX, indexY)} className='w-9 h-9 border'>{axisX}</p> : <p key={`empty-${indexX}-${indexY}`} className='w-9 h-9 border bg-slate-300'></p>
-    }
-  }), [matrix])
-
-  const CalendaryMap = useCallback(() => 
-  matrix.map((axisY, indexY) => {
-    return ( 
-      <div key={`row-${indexY}`} className='flex flex-row'>
-        <p className='w-9 h-9 border text-center flex justify-center items-center'>{indexY}</p>
-        <div className='flex flex-row'>
-         <CalendaryCell axisY={axisY} indexY={indexY}/>
-        </div>
-      </div>
-    )
-  }), [matrix])
+  const CalendaryMap = useCallback(
+    () =>
+      matrix.map((axisY, indexY) => {
+        return (
+          <div key={`row-${indexY}`} className="flex flex-row">
+            <p className="w-9 h-9 border text-center flex justify-center items-center">
+              {indexY}
+            </p>
+            <div className="flex flex-row">
+              <CalendaryCell axisY={axisY} indexY={indexY} />
+            </div>
+          </div>
+        );
+      }),
+    [matrix]
+  );
 
   return (
     <>
-      <div className='flex flex-col'>
-        <CalendaryMap/> 
+      <div className="flex flex-col">
+        <CalendaryMap />
       </div>
     </>
-    )
-  }
+  );
+}
 
-export default Calendary
+export default Calendary;
